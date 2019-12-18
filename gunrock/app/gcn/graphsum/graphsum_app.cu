148d80de4105cd897f01d4e7cb581d87f19fbf0d
woshinande <e0134149@u.nus.edu>
2019-12-18 19:11:19 +0800

    interfacing graphsum with parallel-gcn

--- gunrock/app/gcn/graphsum/graphsum_app.cu
+++ gunrock/app/gcn/graphsum/graphsum_app.cu
@@ -201,7 +201,7 @@ info.ComputeTraversalStats(enactor, (VertexT *)NULL);
 }  // namespace app
 }  // namespace gunrock
 
-/*
+/**
  * @brief Entry of gunrock_sssp function
  * @tparam     GraphT     Type of the graph
  * @tparam     ValueT     Type of the ranking values
@@ -256,7 +256,7 @@ double gunrock_pagerank(gunrock::util::Parameters &parameters, GraphT &graph,
   return total_time;
 }
 
-/*
+/**
  * @brief Simple interface take in graph as CSR format
  * @param[in]  num_nodes   Number of veritces in the input graph
  * @param[in]  num_edges   Number of edges in the input graph
@@ -328,51 +328,6 @@ double pagerank(const SizeT num_nodes, const SizeT num_edges,
   // srcs.clear();
 
   return elapsed_time;
-}
-
-/*
- * @brief Simple interface take in graph as CSR format
- * @param[in]  num_nodes   Number of veritces in the input graph
- * @param[in]  num_edges   Number of edges in the input graph
- * @param[in]  row_offsets CSR-formatted graph input row offsets
- * @param[in]  col_indices CSR-formatted graph input column indices
- * @param[in]  source      Source for personalized PR
- * @param[in]  normalize   Whether to normalize ranking values
- * @param[out] node_ids    Return top-ranked vertex IDs
- * @param[out] pagerank    Return PageRank scores per node
- * \return     double      Return accumulated elapsed times for all runs
- */
-template <typename VertexT = int, typename SizeT = int, typename ValueT = float>
-double pagerank(const SizeT num_nodes, const SizeT num_edges,
-                const SizeT *row_offsets, const VertexT *col_indices,
-                bool normalize, VertexT source, VertexT *node_ids,
-                ValueT *ranks) {
-  if (source == -1) {
-    return pagerank(num_nodes, num_edges, row_offsets, col_indices,
-                    1 /* num_runs */, normalize, (int *)NULL, &node_ids,
-                    &ranks);
-  }
-
-  return pagerank(num_nodes, num_edges, row_offsets, col_indices,
-                  1 /* num_runs */, normalize, &source, &node_ids, &ranks);
-}
-
-/*
- * @brief Simple interface take in graph as CSR format
- * @param[in]  num_nodes   Number of veritces in the input graph
- * @param[in]  num_edges   Number of edges in the input graph
- * @param[in]  row_offsets CSR-formatted graph input row offsets
- * @param[in]  col_indices CSR-formatted graph input column indices
- * @param[in]  normalize   Whether to normalize ranking values
- * @param[out] node_ids    Return top-ranked vertex IDs
- * @param[out] pagerank    Return PageRank scores per node
- * \return     double      Return accumulated elapsed times for all runs
- */
-double pagerank(const int num_nodes, const int num_edges,
-                const int *row_offsets, const int *col_indices, bool normalize,
-                int *node_ids, float *ranks) {
-  return pagerank(num_nodes, num_edges, row_offsets, col_indices, normalize,
-                  (int)-1 /* source */, node_ids, ranks);
 }
 
 // Leave this at the end of the file
